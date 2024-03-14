4e2bd94696505a44426933df8abb9f5b76c15de3
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-03-14 20:34:33 +0900

    :sparkles: 기본 구조 세팅

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by Jia Jang on 3/14/24.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
