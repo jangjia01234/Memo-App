ba2bacf0bb0056691ff80faefc5182b979f2af76
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-03-14 21:04:32 +0900

    :art: 기본 뷰 구현

--- SwiftUIMemo/MainListView.swift
+++ SwiftUIMemo/MainListView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1) 
+                        // 메모 내용을 한 줄만 표시하고 넘어가면 ... 표시
+                    
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
     }
 }
 
 #Preview {
     MainListView()
+        .environmentObject(MemoStore())
 }
