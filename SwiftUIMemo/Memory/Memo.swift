8b995ca1df41b19f293620c08be4b979313cd6fd
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-03-14 20:55:13 +0900

    :seedling: MemoStore 생성

---
+++ SwiftUIMemo/Memory/Memo.swift
@@ -0,0 +1,20 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by Jia Jang on 3/14/24.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
